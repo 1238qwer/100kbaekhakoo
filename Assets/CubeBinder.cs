1499c03b275456960de0cfdb296762da1f06c0cb
1238qwer <1238qwer@naver.com>
2022-06-03 19:01:52 +0900

    3

--- Assets/CubeBinder.cs
+++ Assets/CubeBinder.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class CubeBinder : MonoBehaviour
 {
-    public GameObject[] gameObjects;
+    public Transform[] gameObjects;
     public GameObject particle;
     // Start is called before the first frame update
     void Start()
     {
-        gameObjects = Transform.FindObjectsOfType<GameObject>();
+        //gameObjects = Transform.FindObjectsOfType<GameObject>();
+        gameObjects = GetComponentsInChildren<Transform>();
 
         //GameObject temp = Instantiate(particle, gameObjects[0].transform);
-        //foreach (var item in gameObjects) {
-        //    GameObject temp = Instantiate(particle);
-        //    temp.transform.position = item.transform.position;
-        //}
+        foreach (var item in gameObjects) {
+            if (item.gameObject.activeSelf && item != gameObject.transform) {
+                GameObject temp = Instantiate(particle);
+
+                temp.transform.parent = item.transform;
+                temp.transform.localPosition = Vector3.zero;
+                //temp.transform.position = item.transform.position;
+            }
+        }
     }
 
     // Update is called once per frame
